f15967732f0a40ca599873d1727a532cc3c57c94
Terry Jung <51935603+tj9913@users.noreply.github.com>
2020-09-28 01:08:11 -0700

    Parser completed. Calls correct compute.

--- main.java
+++ main.java
@@ -23,7 +23,8 @@ public class main {
 		values[2] = Integer.MAX_VALUE;//displacement
 		values[3] = Integer.MAX_VALUE;//time
 		
-		int counter = 0;
+		int counter = 0;//count missing variable
+		int counter2 = 0;//count for correct function call
 		String temp;
 		
 		String[] inputMessages = new String[4];
@@ -39,6 +40,7 @@ public class main {
 			System.out.println(inputMessages[i]);//input message displayed
 			temp = console.nextLine();
 			if(temp.equals("q")) {
+				System.out.println("Goodbye");
 				System.exit(0);
 			}
 			else if(isANumber(temp)) {//store valid inputs
@@ -49,23 +51,43 @@ public class main {
 			}
 			else {
 				System.out.println("User input cannot be computed.");
+				return;
+			}
+		}
+		for(counter2 = 0; counter2 < 4; counter2++) {
+			if(Double.compare(values[counter2], Integer.MAX_VALUE) == 0 ) {
 				break;
 			}
+		}
+		if(counter2 == 0) {
+			System.out.println(missing_initVel(values[1],values[2],values[3]));
+		}
+		else if(counter2 == 1) {
+			System.out.println(missing_accel(values[0],values[2],values[3]));
+		}
+		else if(counter2 == 2) {
+			System.out.println(missing_deltaX(values[0],values[1],values[3]));
+		}
+		else if(counter2 == 3) {
+			System.out.println(missing_time(values[0],values[1],values[2]));
+		}
+		else {
+			System.out.println("There is nothing to compute.");
 		}
 	
 	}
 	
 	public static double missing_initVel(double accel, double deltaX, double time) {
-		return 0;
+		return 1;
 	}
 	public static double missing_accel(double accel, double deltaX, double time) {
-		return 0;
+		return 2;
 	}
 	public static double missing_deltaX(double accel, double deltaX, double time) {
-		return 0;
+		return 3;
 	}
 	public static double missing_time(double accel, double deltaX, double time) {
-		return 0;
+		return 4;
 	}
 	
 	public static void main(String[] args) {
@@ -74,5 +96,3 @@ public class main {
 		}
 	}
 }
-
-
